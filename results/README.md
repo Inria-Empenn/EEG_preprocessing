e6589016c986a39f96852a0757ff122ab6bf501a
AyaKabbara <aya.kabbara7@gmail.com>
2022-05-31 08:52:00 +0300

    readme note

---
+++ results/README.md
@@ -0,0 +1,1 @@
+"2" at the end of a filename ==> results for the peak to peak 200 microV
--- src/BST/toset.m
+++ src/BST/toset.m
@@ -1,9 +1,7 @@
-
+% EEGLAB history file generated on the 04-Jan-2022
 % ------------------------------------------------
-%
-
-todir='/Users/ayakabbara/Desktop/projects/EEG_PreProcessing/Raw Data Part 1';
-cd(todir'); %Find and change working folder to raw EEG data
+% 
+cd('/Users/ayakabbara/Desktop/projects/EEG_PreProcessing/Raw Data Part 1'); %Find and change working folder to raw EEG data
 filenames = dir('*.vhdr')
 nb=500;
 for participant = 1:nb %Cycle through participants
@@ -12,8 +10,8 @@ for participant = 1:nb %Cycle through participants
     disp(['Participant: ', num2str(participant)]) %Display current participant being processed
     participant_number = strsplit(filenames(participant).name(1:end-5),'_'); %Split filename into components
     participant_varname = ['set_',participant_number{2}]; %Create new file name
-    EEG = pop_loadbv(to dir, filenames(participant).name);
+    EEG = pop_loadbv('/Users/ayakabbara/Desktop/projects/EEG_PreProcessing/Raw Data Part 1/', filenames(participant).name);
     %ajouter chanlocs
-    EEG= pop_chanedit(EEG, 'lookup',[todir '/supportfiles/Standard-10-20-Cap81.ced']);
-   save([todir '/set/' participant_varname '.set'],'EEG');
+    EEG= pop_chanedit(EEG, 'lookup','/Users/ayakabbara/Desktop/projects/EEG_PreProcessing/eeglab2021.1/functions/supportfiles/Standard-10-20-Cap81.ced');
+    save(['/Users/ayakabbara/Desktop/projects/EEG_PreProcessing/Raw Data Part 1/set/' participant_varname '.set'],'EEG');
 end
