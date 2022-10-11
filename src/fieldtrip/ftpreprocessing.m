a82acbed9d4cf4ccbefdca79ce3b1231e50aedfc
AyaKabbara <aya.kabbara7@gmail.com>
2022-10-11 23:40:51 +0300

    changed path

--- src/fieldtrip/ftpreprocessing.m
+++ src/fieldtrip/ftpreprocessing.m
@@ -1,25 +1,22 @@
 % %  --- This is the preprocessing workflow reproduced by FieldTrip functions---
 % % For contact: aya.kabbara7@gmail.com
 
-%% === Regulate some parameters that will be used in interpolation ====
+cd('../../tools/fieldtrip');
 
-load('src/channelsTokeep.mat');
+%% Regulate some parameters that will be used in interpolation ====
 
 % prepare neighborhood template for bad channels interpolation
 reduced_subjects=ss;
 cfg = [];
-cfg.dataset = '/Raw Data Part 1/set/Subject001/set_001.set';
+cfg.dataset = '../../data/set/Subject001/set_001.set';
 cfg.continuous  = 'yes';              % force it to be continuous
 cfg.channel     = ChannelsTokeep;
 data_prepare  = ft_preprocessing(cfg);
 cfg=[];
 cfg.method ='distance';
-cfg.neighbourdist=0.7;
+cfg.neighbourdist = 0.7;
 [neighbours, cfg] = ft_prepare_neighbours(cfg, data_prepare)
-
-% % go to data folder
-cd('/Raw Data Part 1'); %Find and change working folder to raw EEG data
-filenames = dir('*.vhdr')
+filenames = dir('../../data/*.vhdr')
 nb=500;
 trials_loss=[];
 trials_win=[];
@@ -28,17 +25,13 @@ trials_win=[];
 
 for participant =1:500 %Cycle through participants
     
-%     Get participant name information
+    %%  Get participant name information
     disp(['Participant: ', num2str(participant)]) %Display current participant being processed
     participant_number = strsplit(filenames(participant).name(1:end-5),'_'); %Split filename into components
 
-    RawFile = ['/Raw Data Part 1/set/Subject' participant_number{2} '/set_' participant_number{2} '.set'];
+    RawFile = ['../../data/set/Subject' participant_number{2} '/set_' participant_number{2} '.set'];
     SubjectName = ['participant_' participant_number{2}];
-
-%     % Check if the folder contains the required files
-%     if ~file_exist(RawFile)
-%         error(['The folder does not contain the folder from the file sample.']);
-%     end
+    
     %% === Read data ====
     cfg = [];
     cfg.dataset = RawFile;
@@ -55,7 +48,7 @@ for participant =1:500 %Cycle through participants
 
     %% === Remove the reference channels ====
     cfg=[];
-  if(length(find(reduced_subjects==participant))==0)
+    if(length(find(reduced_subjects==participant))==0)
     cfg.channel     = [1:29];
     else
     cfg.channel     = [1:9 11:20 22:31];
@@ -88,14 +81,12 @@ for participant =1:500 %Cycle through participants
         end
     end
 
-
     %% === Interpolate bad channels ====
     cfg=[];
     cfg.badchannel     = bad_detected;
     cfg.neighbours     = neighbours(index_bad_detected);
     try
     [data_interp] = ft_channelrepair(cfg, data_eeg_filtered)
-
     %% === Segmentation into time-locked epochs ====
     cfg = [];
     cfg.dataset= RawFile;
@@ -126,69 +117,73 @@ for participant =1:500 %Cycle through participants
     cfg=[];
     cfg.trl=cfg_loss.trl;
     cfg.continuous = 'no';
-    cfg.artfctdef.threshold.range=200;
-%     cfg.artfctdef.threshold.min       = -50;
-%     cfg.artfctdef.threshold.max       = 50;
+    cfg.artfctdef.threshold.range=100;
     cfg.artfctdef.threshold.bpfilter  = 'yes';
     cfg.artfctdef.threshold.bpfreq    = [0.1 30];
     [cfg, artifact] = ft_artifact_threshold(cfg, data_loss_corrected)
 
     % % artifact rejection for loss condition
     try
-    data_loss_final = ft_rejectartifact(cfg, data_loss_corrected)
+        data_loss_final = ft_rejectartifact(cfg, data_loss_corrected)
     catch
     end
     % % win condition
     cfg=[];
     cfg.trl=cfg_win.trl;
     cfg.continuous = 'no';
-    cfg.artfctdef.threshold.range=100;
-%     cfg.artfctdef.threshold.min       = -50;
-%     cfg.artfctdef.threshold.max       = 50;
+    cfg.artfctdef.threshold.range=300;
     cfg.artfctdef.threshold.bpfilter  = 'yes';
     cfg.artfctdef.threshold.bpfreq    = [0.1 30];
     [cfg, artifact] = ft_artifact_threshold(cfg, data_win_corrected)
+    
     % % artifact rejection for win condition
-  try
-    data_win_final = ft_rejectartifact(cfg, data_win_corrected)
-  catch
-  end
-    %% === ERP calculation ====
-    cfg=[];
     try
-    [timelock] = ft_timelockanalysis(cfg, data_loss_final);
-    trials_loss(participant)=length(data_loss_final.trial);
-    All_ERP_ft(1,:,:,participant) = timelock.avg;
+        data_win_final = ft_rejectartifact(cfg, data_win_corrected)
     catch
     end
     
-
+    %% === ERP calculation ====
+    cfg=[];
     try
-    [timelock] = ft_timelockanalysis(cfg, data_win_final);
-    trials_win(participant)=length(data_win_final.trial);
-    All_ERP_ft(2,:,:,participant) = timelock.avg;
+        [timelock] = ft_timelockanalysis(cfg, data_loss_final);
+        trials_loss(participant)=length(data_loss_final.trial);
+        All_ERP_ft(2,:,:,participant) = timelock.avg;
+
+        [timelock] = ft_timelockanalysis(cfg, data_win_final);
+        trials_win(participant)=length(data_win_final.trial);
+        All_ERP_ft(1,:,:,participant) = timelock.avg;
+
+        All_trials(1,participant) = trials_win(participant); %Store the number of trials
+        All_trials(2,participant) = trials_loss(participant); %Store the number of trials
+        All_trials(3,participant) = trials_loss(participant)+trials_win(participant); %Store the number of trials
+
     catch
     end
+    
     catch
     end
+    
 end
 
 %% === Save variables and csv files ====
+All_ERP=All_ERP_ft;
+save('../../results/ft_All_ERP', 'All_ERP'); %Save ERP Data
+save('../../results/ft_All_trials', 'All_trials'); %Save ERP Data
+save('../../results/ft_All_rejChan', 'All_rejChan'); %Save ERP Data
 
- save('All_ERP_ft.mat','All_ERP_ft');
 channelOfInterest=26;
 
 All_ERP_ft=All_ERP_ft(:,:,151:750,:);
 % %% RewP_Waveforms_AllPs
-tt1=squeeze(All_ERP_ft(1,26,:,:));
-tt2=squeeze(All_ERP_ft(2,26,:,:));
+win_erp=squeeze(All_ERP_ft(1,26,:,:));
+loss_erp=squeeze(All_ERP_ft(2,26,:,:));
 
-csvwrite('ft_RewP_Waveforms_final22.csv',[(-200:2:998)',nanmean(squeeze(All_ERP_ft(1,26,:,:)),2),nanmean(squeeze(All_ERP_ft(2,26,:,:)),2),nanmean(squeeze(All_ERP_ft(1,26,:,:)),2)-nanmean(squeeze(All_ERP_ft(2,26,:,:)),2)]); %Export data. Conditions: Time, Loss, Win, Difference. Electrode 26 is FCz.
+csvwrite('../../results/ft_RewP_Waveforms.csv',[(-200:2:998)',nanmean(squeeze(All_ERP_ft(1,26,:,:)),2),nanmean(squeeze(All_ERP_ft(2,26,:,:)),2),nanmean(squeeze(All_ERP_ft(1,26,:,:)),2)-nanmean(squeeze(All_ERP_ft(2,26,:,:)),2)]); %Export data. Conditions: Time, Loss, Win, Difference. Electrode 26 is FCz.
 
-csvwrite('ft_RewP_Waveforms_AllPs_final22.csv',[tt1,tt2]'); %Export data. Conditions: Loss, Win. Electrode 26 is FCz.
+csvwrite('../../results/ft_RewP_Waveforms_AllPs.csv',[win_erp,loss_erp]'); %Export data. Conditions: Loss, Win. Electrode 26 is FCz.
 % % %% RewP_Latency
 %
 [~,peak_loc] = max(squeeze(All_ERP_ft(1,26,226:276,:))-squeeze(All_ERP_ft(2,26,226:276,:))); %Determine where the peak amplitude is for each participant. Electrode 26 is FCz.
 peak_loc = (((peak_loc+225)*2)-200)/1000; %Convert into seconds
-csvwrite('ft_RewP_Latency.csv',peak_loc'); %Export data
+csvwrite('../../results/ft_RewP_Latency.csv',peak_loc'); %Export data
 
