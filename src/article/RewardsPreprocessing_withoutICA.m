3fddb65095df15376977ade4912ef58f8f8ed33d
AyaKabbara <aya.kabbara7@gmail.com>
2022-10-12 10:39:48 +0300

    paths fixed

--- src/article/RewardsPreprocessing_withoutICA.m
+++ src/article/RewardsPreprocessing_withoutICA.m
@@ -1,11 +1,9 @@
   % %% PREPROCESSING
  % %% Stage 1: Process data to determine noisy/faulty electrodes
  % %%  Step 1.1: Pre-ICA
- clear all;
- dirdata='data/Raw Data Part 1';
- cd(dirdata); %Find and change working folder to raw EEG data
- filenames = dir('*.vhdr');
-
+cd('../../tools/reference');
+filenames = dir('../../data/*.vhdr')
+dirdata='../../data/';
  for participant = 1:500 %Cycle through participants
 
      %Get participant name information
@@ -15,7 +13,7 @@
      
      %Load Data
      EEG = []; %Clear past data
-     [EEG] = doLoadBVData(filenames(participant).name  ); %Load raw EEG data
+     [EEG] = doLoadBVData([dirdata filenames(participant).name]  ); %Load raw EEG data
      
      %Make it a 32 channels cap - reduces any participants with a 64 channel system
      if EEG.nbchan > 31 %Determine whether current participant is a 64 channel setup
@@ -24,17 +22,13 @@
      
      %Re-Reference
      load('ChanlocsMaster.mat'); %Load channel location file, please make sure the location of this file is in your path
- %     chanlocsMaster([10,21]) = []; %Remove channels TP9 and TP10 as they will be the references
      [EEG] = doRereference(EEG,{'TP9','TP10'},{'ALL'},EEG.chanlocs); %Reference all channels to an averaged TP9/TP10 (mastoids)
      [EEG] = doRemoveChannels(EEG,{'TP9','TP10'},EEG.chanlocs); %Remove reference channels from the data
      [EEG] = doInterpolate(EEG,chanlocsMaster,'spherical'); %Interpolate the electrode used as reference during recording (AFz)
 
      %Filter
      [EEG] = doFilter(EEG,0.1,30,4,60,EEG.srate); %Filter data: Low cutoff 0.1, high cutoff 30, order 4, notch 60
-     
-%      %ICA
- %     [EEG] = doICA(EEG,1); %Run ICA for use of eye blink removal
-    
+         
      %Segment Data  
      [EEG] = doSegmentData(EEG,{'S110','S111'},[-500 1498]); %Segment Data (S110 = Loss, S111 = Win)
      
@@ -52,21 +46,16 @@
      trials_removed = sum(AR_indx')/size(AR_indx,2); %Determine the percentage of segments removed for each electrode
     
      %Save rejection information into a variable: electrodes with a trial rejection rate greater than 40% were tagged for removal
-      p_chanreject{participant,1} = cellstr(participant_number{2}); %Insert participant number
-      p_chanreject{participant,2} = cellstr(num2str(0.4)); %insert lowest level within which less than ten electrodes have been removed
-      p_chanreject{participant,3} = cellstr(num2str(find(trials_removed>0.4))); %Determine the indices of electrodes to be removed      
+     p_chanreject{participant,1} = cellstr(participant_number{2}); %Insert participant number
+     p_chanreject{participant,2} = cellstr(num2str(0.4)); %insert lowest level within which less than ten electrodes have been removed
+     p_chanreject{participant,3} = cellstr(num2str(find(trials_removed>0.4))); %Determine the indices of electrodes to be removed      
 
-     %Save Output
-%      save(participant_varname,'EEG'); %Save the current output so that the lengthy ICA process can run without user intervention
  end
-    %Save channels to reject information into a mat file
+%Save channels to reject information into a mat file
  save('Chans_rejected500.mat','p_chanreject');
  
  %% Stage 2: Process data for analysis
- clear all; close all; clc; %First, clean the environment
-%  dirdata='data/Raw Data Part 1';
-%  cd(dirdata); %Find and change working folder to raw EEG data
- filenames = dir('*.vhdr'); %Compile list of all data
+filenames = dir('../../data/*.vhdr')
 load('Chans_rejected500.mat');
 
  %Load list of electrodes to remove
@@ -107,13 +96,13 @@ load('Chans_rejected500.mat');
 %      end
         
      %Remove faulty electrodes
-    p_chans = []; %Clear past participants electrode indices
-    p_chans = strsplit(p_chanreject{participant,3}{1},' '); %Extract the indices of electrodes to remove
-    %     save the number of channels removed/interpolated
-    All_rejChan(participant)=length(p_chans);
+     p_chans = []; %Clear past participants electrode indices
+     p_chans = strsplit(p_chanreject{participant,3}{1},' '); %Extract the indices of electrodes to remove
+     %     save the number of channels removed/interpolated
+     All_rejChan(participant)=length(p_chans);
 
-    chans_to_remove = []; %Clear past participants electrode labels
-    if ~isempty(p_chans{1}) %Determine whether electrodes need to be removed
+     chans_to_remove = []; %Clear past participants electrode labels
+     if ~isempty(p_chans{1}) %Determine whether electrodes need to be removed
         x = 1; %Begin index count
         for l = 1:size(p_chans,2) %Scroll through the electrodes
             if str2num(p_chans{1,l}) ~= 26 %Ensure that FCz is not removed
@@ -122,9 +111,9 @@ load('Chans_rejected500.mat');
             end
         end
         [EEG] = doRemoveChannels(EEG,chans_to_remove,chanlocsMaster); %Remove electrodes
-    else %If there is no electrodes to remove
+     else %If there is no electrodes to remove
         [EEG] = doRemoveChannels(EEG,{},chanlocsMaster); %Still run function, but do not remove any electrodes
-    end
+     end
     
      
      %  This manual removal has been removed from this code as well as in other software codes    
@@ -142,15 +131,12 @@ load('Chans_rejected500.mat');
 
      %Filter
      [EEG] = doFilter(EEG,0.1,30,4,60,EEG.srate); %Filter da ta: Low cutoff 0.1, high cutoff 30, order 4, notch 60
-     
-     %ICA
- %     [EEG] = doICA(EEG,1); %Run ICA for use of eye blink removal
-     
+          
      %Topograpic Interpolation
      load('ChanlocsMaster.mat'); %Load channel location file, please make sure the location of this file is in your path
      [EEG] = doInterpolate(EEG,chanlocsMaster,'spherical'); %Interpolate electrodes which were previously removed
 
-    %Determine markers of interest
+     %Determine markers of interest
      markers = {'S110','S111'}; %Loss, win
      
      %Segment Data
@@ -183,19 +169,19 @@ load('Chans_rejected500.mat');
      save(participant_varname,'EEG'); %Save the current output so that the lengthy ICA process can run without user intervention
  end
  
- save('Ref_All_ERP', 'All_ERP'); %Save ERP Data
- save('All_trials', 'All_trials'); %Save ERP Data
- save('All_rejChan', 'All_rejChan'); %Save ERP Data
+save('../../results/Ref_All_ERP', 'All_ERP'); %Save ERP Data
+save('../../results/Ref_All_trials', 'All_trials'); %Save ERP Data
+save('../../results/Ref_All_rejChan', 'All_rejChan'); %Save ERP Data
 
-%  % %% RewP_Waveforms
-%  csvwrite('Ref_RewP_Waveforms.csv',[(-200:2:998)',nanmean(squeeze(All_ERP(26,:,1,:)),2),nanmean(squeeze(All_ERP(26,:,2,:)),2),nanmean(squeeze(All_ERP(26,:,1,:)),2)-nanmean(squeeze(All_ERP(26,:,2,:)),2)]); %Export data. Conditions: Time, Loss, Win, Difference. Electrode 26 is FCz.
-%  % %% RewP_Waveforms_AllPs
-%  tt1=squeeze(All_ERP(26,:,1,:));
-%  tt2=squeeze(All_ERP(26,:,2,:));
-% 
-%  csvwrite('Ref_RewP_Waveforms_AllPs.csv',[tt1,tt2]'); %Export data. Conditions: Loss, Win. Electrode 26 is FCz.
-%  % %% RewP_Latency
-%  [~,peak_loc] = max(squeeze(All_ERP(26,226:276,1,:))-squeeze(All_ERP(26,226:276,2,:))); %Determine where the peak amplitude is for each participant. Electrode 26 is FCz.
-%  peak_loc = (((peak_loc+225)*2)-200)/1000; %Convert into seconds
-%  peak_loc(toberemoved)=[];
-%  csvwrite('Ref_RewP_Latency.csv',peak_loc'); %Export data
+%% RewP_Waveforms
+csvwrite('../../results/Ref_RewP_Waveforms.csv',[(-200:2:998)',nanmean(squeeze(All_ERP(26,:,1,:)),2),nanmean(squeeze(All_ERP(26,:,2,:)),2),nanmean(squeeze(All_ERP(26,:,1,:)),2)-nanmean(squeeze(All_ERP(26,:,2,:)),2)]); %Export data. Conditions: Time, Loss, Win, Difference. Electrode 26 is FCz.
+%% RewP_Waveforms_AllPs
+win_erp=squeeze(All_ERP(26,:,1,:));
+loss_erp=squeeze(All_ERP(26,:,2,:));
+
+csvwrite('../../results/Ref_RewP_Waveforms_AllPs.csv',[win_erp,loss_erp]'); %Export data. Conditions: Loss, Win. Electrode 26 is FCz.
+%% RewP_Latency
+[~,peak_loc] = max(squeeze(All_ERP(26,226:276,1,:))-squeeze(All_ERP(26,226:276,2,:))); %Determine where the peak amplitude is for each participant. Electrode 26 is FCz.
+peak_loc = (((peak_loc+225)*2)-200)/1000; %Convert into seconds
+peak_loc(toberemoved)=[];
+csvwrite('../../results/Ref_RewP_Latency.csv',peak_loc'); %Export data
